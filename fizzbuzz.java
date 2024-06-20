936a111d497dc76bb080968f3c62fa1aaec74ea6
mugisha-rwilliam <137517138+mugisha-rwilliam@users.noreply.github.com>
2024-06-20 17:29:10 +0200

    Update fizzbuzz.java v1

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,1 +1,36 @@
+public class FizzBuzz {
+    
+}
+import java.util.Scanner;
 
+public class FizzBuzzCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Please enter a number: ");
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+
+                if (number % 3 == 0) {
+                    if (number % 5 == 0) {
+                        System.out.println("FizzBuzz");
+                        return; // Exit the method
+                    } else {
+                        System.out.println("Fizz");
+                        return; // Exit the method
+                    }
+                } else if (number % 5 == 0) {
+                    System.out.println("Buzz");
+                    return; // Exit the method
+                } else {
+                    System.out.println("Please try again.");
+                }
+            } else {
+                System.out.println("Error: Please enter a valid number.");
+                scanner.next(); // Clear the invalid input
+            }
+        }
+        scanner.close():
+    }
+}
